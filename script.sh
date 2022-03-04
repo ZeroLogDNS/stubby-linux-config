3b192a761d3de263fcf624b9bbe3c0e8e228dee7
Kristóf <55440418+libDarkstreet@users.noreply.github.com>
2022-03-04 17:32:32 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -55,6 +55,9 @@ detect_os()
         elif [ "$ID_LIKE" = "rhel fedora" ]; then
             msg "Installing Stubby for CentOS/Fedora"
             dnf install stubby -y && response="found"
+	elif [ "$ID" = "fedora" ]; then
+            msg "Installing Stubby for Fedora"
+            dnf install stubby -y && response="found"
         elif [ "$ID" = "arch" ]; then
             pacman -S stubby --noconfirm && response="found"
             msg "Installing Stubby for Arch Linux"
